fc2de07f473617c09911e9b5650f481ac5840b75
MrTimwilzs <77224061+MrTimwilzs@users.noreply.github.com>
2021-01-13 02:16:36 +0000

    Add files via upload

---
+++ layer/layer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Layer</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="img1" style="background-image: url(img/mazi.png); "> 
+</div> 
+<div id="img2" style="background-image: url(img/kenny.png); "> 
+</div> 
+<div id="img3" style="background-image: url(img/akale.png); "> 
+</div>  
+</body>
+</body>
+</html>
